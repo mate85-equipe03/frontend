7c9dff90a5ec4d38158510a8cf7cbf0374b609be
Lucas Morais <64046792+mblucs@users.noreply.github.com>
2022-09-11 14:03:26 -0300

    Add footer logos (#12)

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import pgcomp from "../assets/pgcomp-horizontal.png";
 
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="footer">
+      <div id="contato-ufba">
+        <div>
+          <strong>INSTITUTO DE COMPUTAÇÃO</strong>
+        </div>
+        <div>
+          Avenida Milton Santos, s/n - Campus de Ondina, PAF 2 - Salvador -
+          Bahia, CEP 40.170-110
+        </div>
+        <div>
+          E-mail: <a href="mailto:ceapg-ic@ufba.br">ceapg-ic@ufba.br</a>
+        </div>
+      </div>
+      <div>
+        <a href="https://pgcomp.ufba.br/" target="_blank" rel="noreferrer">
+          <img src={pgcomp} alt="PGCOMP UFBA" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
