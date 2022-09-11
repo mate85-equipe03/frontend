fe8e5107815d413cdc2061dcde12d93341acb594
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2022-09-11 13:21:41 -0300

    lint fix

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,17 +5,26 @@ function Footer() {
   return (
     <footer>
       <div id="contato-ufba">
-        <div> <strong>INSTITUTO DE COMPUTAÇÃO</strong></div>
-        <div> Avenida Milton Santos, s/n - Campus de Ondina, PAF 2 - Salvador - Bahia, CEP 40.170-110 </div>
-        <div> E-mail: <a href="mailto:ceapg-ic@ufba.br">ceapg-ic@ufba.br</a></div>
+        <div>
+          {" "}
+          <strong>INSTITUTO DE COMPUTAÇÃO</strong>
+        </div>
+        <div>
+          {" "}
+          Avenida Milton Santos, s/n - Campus de Ondina, PAF 2 - Salvador -
+          Bahia, CEP 40.170-110{" "}
+        </div>
+        <div>
+          {" "}
+          E-mail: <a href="mailto:ceapg-ic@ufba.br">ceapg-ic@ufba.br</a>
+        </div>
       </div>
 
       <div>
-        <a href="https://pgcomp.ufba.br/" target="_blank">
+        <a href="https://pgcomp.ufba.br/" target="_blank" rel="noreferrer">
           <img src={pgcomp} alt="PGCOMP UFBA" />
         </a>
       </div>
-
     </footer>
   );
 }
