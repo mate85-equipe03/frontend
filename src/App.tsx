6c988006b03859104ba057f53d7e14077f9c6508
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2022-09-11 23:43:51 -0300

    login com material

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./Components/Auth";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Grid from "@mui/material/Grid";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./Theme";
 
 function App() {
   return (
-    <div className="app">
+    <ThemeProvider theme={theme}>
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </BrowserRouter>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        p={3}
+      >
+        <Grid item>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/auth" element={<Auth />} />
+            </Routes>
+          </BrowserRouter>
+        </Grid>
+      </Grid>
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Auth.tsx
+++ src/Components/Auth.tsx
@@ -1,53 +1,142 @@
-import React from "react";
-import logoPosgress from "../assets/posgress-vertical.png";
+import * as React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Link,
+  OutlinedInput,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { InfoOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface FormLogin {
+  login: string;
+  password: string;
+  showPassword: boolean;
+}
 
 export default function Login() {
+  const [values, setValues] = React.useState<FormLogin>({
+    login: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop: keyof FormLogin) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({
+        ...values,
+        [prop]: event.target.value,
+      });
+    };
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const sendForm = () => {
+    console.log(values);
+  };
+
   return (
-    <div className="auth-form-container">
-      <img src={logoPosgress} alt="Logo do Sistema" />
-      <form className="auth-form" id="form-login">
-        <div className="auth-form-content">
-          <h3 className="auth-form-title">Login</h3>
-          <div className="form-group mt-3">
-            <label id="form" htmlFor="matricula">
-              Matrícula
-              <input
-                id="matricula"
-                type="text"
-                className="form-control mt-1"
-                placeholder="Digite sua matrícula"
-              />
-            </label>
-          </div>
-          <div className="form-group mt-3">
-            <label id="form" htmlFor="senha">
-              Password
-              <input
-                id="senha"
-                type="password"
-                className="form-control mt-1"
-                placeholder="Digite sua senha"
-              />
-            </label>
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="button-login">
-              Logar
-            </button>
-          </div>
-          <p id="cadastro" className="forgot-password text-right mt-2">
-            Não tem conta?
-            <a className="link-login" href="#cadastro">
-              Cadastre-se
-            </a>
-          </p>
-          <p id="esqueci" className="forgot-password text-right mt-2">
-            <a className="link-login" href="#esqueci">
-              Esqueci a senha
-            </a>
-          </p>
-        </div>
-      </form>
-    </div>
+    <Card sx={{ minWidth: 275, maxWidth: 500 }}>
+      <CardHeader
+        title="Login"
+        titleTypographyProps={{
+          align: "center",
+          variant: "h4",
+          p: 1,
+        }}
+      />
+
+      <Divider sx={{ mx: 3 }} />
+
+      <CardContent sx={{ px: 10 }}>
+        <FormControl required fullWidth margin="normal">
+          <InputLabel htmlFor="login">Login</InputLabel>
+          <OutlinedInput
+            id="login"
+            label="Login"
+            placeholder="Digite seu login"
+            type="text"
+            value={values.login}
+            onChange={handleChange("login")}
+            endAdornment={
+              <InputAdornment position="end">
+                <Tooltip title="Discentes devem informar a matrícula. Docentes devem informar o SIAPE.">
+                  <IconButton>
+                    <InfoOutlined />
+                  </IconButton>
+                </Tooltip>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl required fullWidth margin="normal">
+          <InputLabel htmlFor="senha">Senha</InputLabel>
+          <OutlinedInput
+            id="senha"
+            label="Senha"
+            placeholder="Digite sua senha"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    (values.showPassword ? "Ocultar" : "Mostrar") + "senha"
+                  }
+                  onClick={handleClickShowPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </CardContent>
+
+      <CardActions sx={{ px: 10 }}>
+        <Grid container justifyContent="center" alignItems="center">
+          <Button
+            fullWidth
+            type="submit"
+            onClick={sendForm}
+            variant="contained"
+            size="large"
+          >
+            Login
+          </Button>
+        </Grid>
+      </CardActions>
+
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        className="text-sm"
+        sx={{ mt: 3, p: 2, backgroundColor: "primary.light" }}
+      >
+        <Typography>
+          Não tem conta? <Link href="#">Cadastre-se</Link>
+        </Typography>
+        <Link href="#">Esqueci minha senha</Link>
+      </Grid>
+    
+    </Card>
   );
 }
