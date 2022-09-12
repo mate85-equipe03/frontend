20381639a9af3f916459404da5bac72faee22b17
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2022-09-12 00:23:46 -0300

    lint fix

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -53,97 +53,97 @@ export default function Login() {
 
   return (
     <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        sx={{ height: "100%" }}
-      >
-    <Card sx={{ minWidth: 275, maxWidth: 500 }}>
-      <CardHeader
-        title="Login"
-        titleTypographyProps={{
-          align: "center",
-          variant: "h4",
-          p: 1,
-        }}
-      />
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ height: "100%" }}
+    >
+      <Card sx={{ minWidth: 275, maxWidth: 500 }}>
+        <CardHeader
+          title="Login"
+          titleTypographyProps={{
+            align: "center",
+            variant: "h4",
+            p: 1,
+          }}
+        />
 
-      <Divider sx={{ mx: 3 }} />
+        <Divider sx={{ mx: 3 }} />
 
-      <CardContent sx={{ px: 10 }}>
-        <FormControl required fullWidth margin="normal">
-          <InputLabel htmlFor="login">Login</InputLabel>
-          <OutlinedInput
-            id="login"
-            label="Login"
-            placeholder="Digite seu login"
-            type="text"
-            value={values.login}
-            onChange={handleChange("login")}
-            endAdornment={
-              <InputAdornment position="end">
-                <Tooltip title="Discentes devem informar a matrícula. Docentes devem informar o SIAPE.">
-                  <IconButton>
-                    <InfoOutlined />
+        <CardContent sx={{ px: 10 }}>
+          <FormControl required fullWidth margin="normal">
+            <InputLabel htmlFor="login">Login</InputLabel>
+            <OutlinedInput
+              id="login"
+              label="Login"
+              placeholder="Digite seu login"
+              type="text"
+              value={values.login}
+              onChange={handleChange("login")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <Tooltip title="Discentes devem informar a matrícula. Docentes devem informar o SIAPE.">
+                    <IconButton>
+                      <InfoOutlined />
+                    </IconButton>
+                  </Tooltip>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl required fullWidth margin="normal">
+            <InputLabel htmlFor="senha">Senha</InputLabel>
+            <OutlinedInput
+              id="senha"
+              label="Senha"
+              placeholder="Digite sua senha"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={`${
+                      values.showPassword ? "Ocultar" : "Mostrar"
+                    }senha`}
+                    onClick={handleClickShowPassword}
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
                   </IconButton>
-                </Tooltip>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <FormControl required fullWidth margin="normal">
-          <InputLabel htmlFor="senha">Senha</InputLabel>
-          <OutlinedInput
-            id="senha"
-            label="Senha"
-            placeholder="Digite sua senha"
-            type={values.showPassword ? "text" : "password"}
-            value={values.password}
-            onChange={handleChange("password")}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label={`${
-                    values.showPassword ? "Ocultar" : "Mostrar"
-                  }senha`}
-                  onClick={handleClickShowPassword}
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-      </CardContent>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </CardContent>
 
-      <CardActions sx={{ px: 10 }}>
-        <Grid container justifyContent="center" alignItems="center">
-          <Button
-            fullWidth
-            type="submit"
-            onClick={sendForm}
-            variant="contained"
-            size="large"
-          >
-            Login
-          </Button>
+        <CardActions sx={{ px: 10 }}>
+          <Grid container justifyContent="center" alignItems="center">
+            <Button
+              fullWidth
+              type="submit"
+              onClick={sendForm}
+              variant="contained"
+              size="large"
+            >
+              Login
+            </Button>
+          </Grid>
+        </CardActions>
+
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          className="text-sm"
+          sx={{ mt: 3, p: 2, backgroundColor: "primary.light" }}
+        >
+          <Typography>
+            Não tem conta? <Link href="#login">Cadastre-se</Link>
+          </Typography>
+          <Link href="#senha">Esqueci minha senha</Link>
         </Grid>
-      </CardActions>
-
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        className="text-sm"
-        sx={{ mt: 3, p: 2, backgroundColor: "primary.light" }}
-      >
-        <Typography>
-          Não tem conta? <Link href="#login">Cadastre-se</Link>
-        </Typography>
-        <Link href="#senha">Esqueci minha senha</Link>
-      </Grid>
-    </Card>
+      </Card>
     </Grid>
   );
 }
