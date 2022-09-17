e1644ec3e3cb6dd7914af8c5fc052158b6658ebe
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2022-09-17 16:20:08 -0300

    layout tela

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Button, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  List,
+  ListSubheader,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 interface Props {
@@ -21,11 +33,100 @@ export default function Home({ user, setUser }: Props) {
   return (
     <Grid
       container
-      direction="row"
+      direction="column"
       justifyContent="center"
       alignItems="center"
       sx={{ height: "100%" }}
     >
+      <Card sx={{ minWidth: 500, maxWidth: 800 }}>
+        <CardHeader
+          title="Processos Seletivos"
+          titleTypographyProps={{
+            align: "center",
+            variant: "h4",
+            p: 1,
+          }}
+          subheader="Concessão de Bolsas de Mestrado e Doutorado"
+          subheaderTypographyProps={{
+            align: "center",
+          }}
+        />
+
+        <Divider sx={{ mx: 3 }} />
+
+        <CardContent sx={{ px: 4 }}>
+          <List
+            component="nav"
+            aria-labelledby="editais-em-andamento"
+            subheader={
+              <ListSubheader
+                id="editais-em-andamento"
+                color="primary"
+                sx={{
+                  fontSize: 20,
+                }}
+              >
+                Em andamento
+              </ListSubheader>
+            }
+          >
+            <ListItemButton sx={{ mx:2}} divider>
+              <ListItemText>
+                <Grid>
+                  <Typography sx={{ fontSize: 14 }}>
+                    Edital PGCOMP-mm/aaaaa
+                  </Typography>
+                  <Typography sx={{ fontSize: 10 }}>
+                    Inscrições abertas - até dd/mm/aaaa
+                  </Typography>
+                </Grid>
+              </ListItemText>
+            </ListItemButton>
+
+
+            <ListItemButton sx={{ mx:2}} divider>
+              <ListItemText>
+                <Typography sx={{ fontSize: 14 }}>
+                  Edital PGCOMP-mm/aaaaa
+                </Typography>
+                <Grid container direction="row" justifyContent="space-between">
+                  <Typography sx={{ fontSize: 10 }}>
+                    Inscrições abertas - até dd/mm/aaaa
+                  </Typography>
+                </Grid>
+              </ListItemText>
+            </ListItemButton>
+          </List>
+
+          <List
+            component="nav"
+            aria-labelledby="editais-encerrados"
+            subheader={
+              <ListSubheader
+                id="editais-encerrados"
+                color="primary"
+                sx={{ fontSize: 20 }}
+              >
+                Encerrados
+              </ListSubheader>
+            }
+          >
+          <ListItemButton sx={{ mx:2}} divider>
+              <ListItemText>
+                <Typography sx={{ fontSize: 14 }}>
+                  Edital PGCOMP-mm/aaaaa
+                </Typography>
+                <Grid container direction="row" justifyContent="space-between">
+                  <Typography sx={{ fontSize: 10 }}>
+                    Resultados disponíves
+                  </Typography>
+                </Grid>
+              </ListItemText>
+            </ListItemButton>
+          </List>
+        </CardContent>
+      </Card>
+
       {user ? (
         <Grid
           container
