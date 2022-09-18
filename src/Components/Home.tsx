10b60a27e5762bc51e144443574842cbf2f67a62
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2022-09-18 12:02:19 -0300

    simulação com dados mockados

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -11,7 +11,6 @@ import {
   ListSubheader,
   ListItemText,
   ListItemButton,
-  Alert,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
@@ -29,6 +28,27 @@ export default function Home({ user, setUser }: Props) {
 
   const redirectToLogin = () => {
     navigate("/login");
+  };
+
+  //Mockup *Temporário*
+  const editais = {
+    em_andamento: [
+      {
+        nome: "Edital PGCOMP-09/2022",
+        etapa: { nome: "Inscrições Abertas", data_fim: "dd/mm/aaaa" },
+        inscrito: true,
+      },
+      {
+        nome: "Edital PGCOMP-07/2022",
+        etapa: { nome: "Divulgação dos Resultados Parciais", data_fim: "dd/mm/aaaa" },
+        inscrito: false,
+      },
+    ],
+    encerrados: [
+      {
+        nome: "Edital PGCOMP-03/2022",
+      },
+    ],
   };
 
   return (
@@ -52,9 +72,7 @@ export default function Home({ user, setUser }: Props) {
             align: "center",
           }}
         />
-
         <Divider sx={{ mx: 3 }} />
-
         <CardContent sx={{ px: 4 }}>
           <List
             component="nav"
@@ -71,42 +89,33 @@ export default function Home({ user, setUser }: Props) {
               </ListSubheader>
             }
           >
-            <ListItemButton sx={{ mx:2}} divider>
-              <ListItemText>
-                <Grid>
-                  <Typography sx={{ fontSize: 14 }}>
-                    Edital PGCOMP-mm/aaaaa
-                  </Typography>
-                  <Grid
-                    container
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="flex-end"
-                    flex-wrap="wrap"
-                  >
-                    <Typography sx={{ fontSize: 10, mr: 2 }}>
-                      Inscrições abertas - Até dd/mm/aaaa
-                    </Typography>
-                    <Typography sx={{ fontSize: 10, color: "success.main" }}>
-                      Você está inscrito.
-                    </Typography>
+            {editais.em_andamento.map((edital) => (
+              <ListItemButton key={edital.nome} sx={{ mx: 2 }} divider>
+                <ListItemText>
+                  <Grid>
+                    <Typography sx={{ fontSize: 14 }}>{edital.nome}</Typography>
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="flex-end"
+                      flex-wrap="wrap"
+                    >
+                      <Typography sx={{ fontSize: 10, mr: 2 }}>
+                        {edital.etapa.nome} - Até {edital.etapa.data_fim}
+                      </Typography>
+                      {edital.inscrito && (
+                        <Typography
+                          sx={{ fontSize: 10, color: "success.main" }}
+                        >
+                          Você está inscrito.
+                        </Typography>
+                      )}
+                    </Grid>
                   </Grid>
-                </Grid>
-              </ListItemText>
-            </ListItemButton>
-
-            <ListItemButton sx={{ mx: 2 }} divider>
-              <ListItemText>
-                <Typography sx={{ fontSize: 14 }}>
-                  Edital PGCOMP-mm/aaaaa
-                </Typography>
-                <Grid container direction="row" justifyContent="space-between">
-                  <Typography sx={{ fontSize: 10 }}>
-                    Inscrições abertas - Até dd/mm/aaaa
-                  </Typography>
-                </Grid>
-              </ListItemText>
-            </ListItemButton>
+                </ListItemText>
+              </ListItemButton>
+            ))}
           </List>
 
           <List
@@ -122,22 +131,30 @@ export default function Home({ user, setUser }: Props) {
               </ListSubheader>
             }
           >
-            <ListItemButton sx={{ mx:2}} divider>
-              <ListItemText>
-                <Typography sx={{ fontSize: 14 }}>
-                  Edital PGCOMP-mm/aaaaa
-                </Typography>
-                <Grid container direction="row" justifyContent="space-between">
-                  <Typography sx={{ fontSize: 10 }}>
-                    Resultados disponíves
+            {editais.encerrados.map((edital) => (
+              <ListItemButton key={edital.nome} sx={{ mx: 2 }} divider>
+                <ListItemText>
+                  <Typography sx={{ fontSize: 14 }}>
+                    {edital.nome}
                   </Typography>
-                </Grid>
-              </ListItemText>
-            </ListItemButton>
+                  <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                  >
+                    <Typography sx={{ fontSize: 10 }}>
+                      Resultados disponíves
+                    </Typography>
+                  </Grid>
+                </ListItemText>
+              </ListItemButton>
+            ))}
           </List>
         </CardContent>
       </Card>
 
+      &nbsp;
+      
       {user ? (
         <Grid
           container
