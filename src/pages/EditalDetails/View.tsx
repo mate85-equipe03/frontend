efde2a3cd6f62649880ab336d46d5963449cc086
Kennedy-Anderson <kennedyandrade98@gmail.com>
2022-09-22 20:28:45 -0300

    Cria a rota para detalhes

---
+++ src/pages/EditalDetails/View.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function EditalDetails(){
+    return(
+        <h1>Hello World</h1>
+    );
+}
--- src/routes/GeneralRoutes.tsx
+++ src/routes/GeneralRoutes.tsx
@@ -3,10 +3,12 @@ import { RouteProps } from "react-router-dom";
 import Home from "../pages/Home/View";
 import PageNotFound from "../pages/PageNotFound/View";
 import { routeWithKey } from "./RoutesHelper";
+import EditalDetails from "../pages/EditalDetails/View";
 
 const generalRoutes: RouteProps[] = [
   { path: "/", element: <Home /> },
   { path: "*", element: <PageNotFound /> },
+  {path: "/detalhes-edital", element: <EditalDetails />},
 ];
 
 const GeneralRoutes = generalRoutes.map((route) => {
