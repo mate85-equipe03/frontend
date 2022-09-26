a6dfe6701dbe11e33b014560072cf7444f0e557d
Kennedy-Anderson <68911962+Kennedy-Anderson@users.noreply.github.com>
2022-09-26 17:15:19 -0300

    Posg 7 detalhes edital (#40)
    * Cria a rota para detalhes
    
    * Cria metodo para clicar nos editais
    
    * lint fix
    
    * Cria requisição para detalhes
    
    * Adiciona passagem de id do edital pela rota e utiliza na requisição
    
    * Cria condições para exibição dos botões
    
    * Adicona componentes MUI na exibição dos detalhes
    
    * lint fix
    
    * Remove comentários
    
    * Atualiza branch com a main
    
    * lint-fix
    
    * Adiciona rota de inscrição ao botão inscreva-se
    
    * Lint-fix
    
    * Ajustes lint

---
+++ src/pages/EditalDetails/Types.tsx
@@ -0,0 +1,15 @@
+export interface IEtapa {
+  id: number;
+  name: string;
+  data_inicio: string;
+  data_fim: string;
+}
+
+export interface IDetails {
+  titulo: string;
+  semestre: string;
+  descricao: string;
+  edital_url: string;
+  arquivado: boolean;
+  etapas: IEtapa[];
+}
