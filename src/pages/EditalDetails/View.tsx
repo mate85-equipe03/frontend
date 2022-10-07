2c39088089c02c67fb04b94e76c6bc8ba3fff9a7
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2022-10-06 21:57:51 -0300

    fix useEffect

--- src/pages/EditalDetails/View.tsx
+++ src/pages/EditalDetails/View.tsx
@@ -43,7 +43,7 @@ export default function EditalDetails() {
       .finally(() => {
         // setLoading(false);
       });
-  });
+  }, []);
 
   const dateToStr = (rawDate: string) => {
     const date = moment(rawDate);
