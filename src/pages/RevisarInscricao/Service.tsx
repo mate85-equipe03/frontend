223b98dbd81cd66e4c260487cb6470afa66dd4cb
Kennedy-Anderson <kennedyandrade98@gmail.com>
2022-10-10 17:09:22 -0300

    Muda ADetalhes para IDetalhes

--- src/pages/RevisarInscricao/Service.tsx
+++ src/pages/RevisarInscricao/Service.tsx
@@ -1,12 +1,12 @@
 import api from "../../services/Api";
-import { ADetalhes } from "./Interfaces";
+import { IDetalhes } from "./Interfaces";
 
 const getDetalhesInscricaoProfessor = (
   inscricaoId: string | undefined,
   editalId: string | undefined,
   token: string | undefined
 ) => {
-  return api.get<ADetalhes>(
+  return api.get<IDetalhes>(
     `/processos-seletivos/${editalId}/inscricoes/${inscricaoId}`,
     {
       headers: {
--- src/pages/RevisarInscricaoAluno/Service.tsx
+++ src/pages/RevisarInscricaoAluno/Service.tsx
@@ -1,11 +1,11 @@
 import api from "../../services/Api";
-import { ADetalhes } from "./Interfaces";
+import { IDetalhes } from "./Interfaces";
 
 const getDetalhesInscricaoAluno = (
   editalId: string | undefined,
   token: string | undefined
 ) => {
-  return api.get<ADetalhes>(`/processos-seletivos/${editalId}/inscricao`, {
+  return api.get<IDetalhes>(`/processos-seletivos/${editalId}/inscricao`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
