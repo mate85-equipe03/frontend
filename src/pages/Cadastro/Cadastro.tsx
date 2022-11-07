8521bf39ec21c2e1563f98df62571da1036aa654
Kennedy-Anderson <kennedyandrade98@gmail.com>
2022-11-07 11:13:23 -0300

    Ajusta useEffect

--- src/pages/Cadastro/Cadastro.tsx
+++ src/pages/Cadastro/Cadastro.tsx
@@ -139,7 +139,7 @@ export default function Cadastro() {
       .finally(() => {
         setLoading(false);
       });
-  }, [user, signUpData]);
+  }, [user]);
 
   return loading ? (
     <Loading />
