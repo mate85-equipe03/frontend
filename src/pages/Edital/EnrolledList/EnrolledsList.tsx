ebc258d2b231cb4be2056e6b94656108a324999c
Kennedy-Anderson <kennedyandrade98@gmail.com>
2022-11-15 16:15:29 -0300

    Ajusta as regras de clique na linha

--- src/pages/Edital/EnrolledList/EnrolledsList.tsx
+++ src/pages/Edital/EnrolledList/EnrolledsList.tsx
@@ -1,7 +1,18 @@
-import { Card, CardContent, CardHeader, Divider, Grid } from "@mui/material";
+import {
+  Alert,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  Snackbar,
+  Tooltip,
+} from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DataGrid, GridColDef, GridEventListener } from "@mui/x-data-grid";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import WarningIcon from "@mui/icons-material/Warning";
 import { getEnrolledList } from "./Service";
 import { IADetalhes } from "./Interfaces";
 import getDetailsProcessoSeletivo from "../Detalhes/Service";
@@ -19,6 +30,7 @@ export default function EnrolledsList() {
   const [enrolledList, setEnrolledList] = useState<IADetalhes[]>([]);
   const [loadingInscritos, setLoadingInscritos] = useState<boolean>(false);
   const [loadingProcesso, setLoadingProcesso] = useState<boolean>(false);
+  const [clickError, setClickError] = useState<string>("");
 
   useEffect(() => {
     if (user && editalId) {
@@ -34,8 +46,42 @@ export default function EnrolledsList() {
     }
   }, [editalId, user, navigate]);
 
+  function isRowClickable(
+    revisor_id: number | null,
+    auditor_id: number | null
+  ) {
+    return !(revisor_id == user?.id);
+  }
+
+  function revisorCellContent(revisor_id: number | null) {
+    return revisor_id ? (
+      <Grid sx={{ color: "success.main", fontWeight: "bold" }}>
+        <CheckCircleIcon fontSize="small" sx={{ mr: 0.5 }} />
+        Revisado
+      </Grid>
+    ) : (
+      <Grid sx={{ color: "warning.light", fontWeight: "bold" }}>
+        <WarningIcon fontSize="small" sx={{ mr: 0.5 }} />A revisar
+      </Grid>
+    );
+  }
+
   const handleRowClick: GridEventListener<"rowClick"> = (params) => {
-    navigate(`/edital/${editalId}/inscritos/${params.row.id}`);
+    if (isRowClickable(params.row.revisor_id, params.row.auditor_id)) {
+      navigate(`/edital/${editalId}/inscritos/${params.row.id}`);
+    } else {
+      setClickError("Auditor(a) não pode ser igual ao(à) revisor(a).");
+    }
+  };
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setClickError("");
   };
 
   useEffect(() => {
@@ -81,25 +127,36 @@ export default function EnrolledsList() {
       field: "statusRevisao",
       headerName: "Status da Revisão",
       width: 150,
-      valueGetter: (params) => {
-        if(params.getValue(params.id, "aluno").revisor_id){
-          return "Revisado";
-        }else{
-          return "Aguardando revisão";
-          //return "Clique para revisar";
-        }       
+      valueGetter: (params) => params.row.revisor_id,
+      renderCell: (cellValues) => {
+        return cellValues.row.revisor_id ? (
+          <Grid sx={{ color: "success.main", fontWeight: "bold" }}>
+            <CheckCircleIcon fontSize="small" sx={{ mr: 0.5 }} />
+            Revisada
+          </Grid>
+        ) : (
+          <Grid sx={{ color: "warning.light", fontWeight: "bold" }}>
+            <WarningIcon fontSize="small" sx={{ mr: 0.5 }} />A revisar
+          </Grid>
+        );
       },
     },
     {
       field: "statusAuditoria",
       headerName: "Status da Auditoria",
       width: 150,
-      valueGetter: (params) => {
-        if(params.getValue(params.id, "aluno").auditor_id){
-          return "Auditado";
-        }else{
-          return "-";
-        } 
+      valueGetter: (params) => params.row.auditor_id,
+      renderCell: (cellValues) => {
+        return cellValues.row.auditor_id ? (
+          <Grid sx={{ color: "success.main", fontWeight: "bold" }}>
+            <CheckCircleIcon fontSize="small" sx={{ mr: 0.5 }} />
+            Auditada
+          </Grid>
+        ) : (
+          <Grid sx={{ color: "warning.light", fontWeight: "bold" }}>
+            <WarningIcon fontSize="small" sx={{ mr: 0.5 }} />A auditar
+          </Grid>
+        );
       },
     },
   ];
@@ -118,6 +175,17 @@ export default function EnrolledsList() {
       alignItems="center"
       sx={{ width: "100%" }}
     >
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={Boolean(clickError)}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {clickError}
+        </Alert>
+      </Snackbar>
+
       <Card sx={{ py: 2, mt: 5 }}>
         <CardHeader
           title="Estudantes Inscritos(as)"
@@ -136,10 +204,26 @@ export default function EnrolledsList() {
         <CardContent sx={{ px: 10 }}>
           <DataGrid
             onRowClick={handleRowClick}
+            disableSelectionOnClick
             rows={enrolledList}
             columns={colunas}
+            getRowClassName={(params) => {
+              if (
+                !isRowClickable(params.row.revisor_id, params.row.auditor_id)
+              ) {
+                return "not-clickable-row";
+              }
+              return "clickable-row";
+            }}
             sx={{
               width: Math.min(allColumnsWidth + 2, 1000),
+              ".not-clickable-row": {
+                ":hover": {
+                  color: "initial",
+                  backgroundColor: "initial",
+                  cursor: "default",
+                },
+              },
             }}
           />
         </CardContent>
