313ffc53a35f27a6a6ed32d4eb08984a23c8b843
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2022-11-19 15:47:33 -0300

    detalhes

--- src/pages/Edital/Inscricao/Service.tsx
+++ src/pages/Edital/Inscricao/Service.tsx
@@ -1,28 +1,7 @@
 import api from "../../../services/Api";
 import { IEditInscricao, IInscricaoDataReq } from "./Interfaces";
 
-export const postInscricao = (payload: IInscricaoDataReq) => {
-  const formData = new FormData();
-  payload.historico_graduacao_file.forEach((file) => {
-    formData.append("historico_graduacao_file", file);
-  });
-  payload.historico_posgraduacao_file.forEach((file) => {
-    formData.append("historico_posgraduacao_file", file);
-  });
-  formData.append("url_enade", payload.url_enade);
-  formData.append(
-    "processo_seletivo_id",
-    payload.processo_seletivo_id.toString()
-  );
-  formData.append("nota_enade", payload.nota_url_enade.toString());
-  return api.post("/inscricoes", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-};
-
-export const patchInscricao = (payload: IInscricaoDataReq) => {
+const formData = (payload: IInscricaoDataReq) => {
   const formData = new FormData();
   payload.historico_graduacao_file.forEach((file) => {
     formData.append("historico_graduacao_file", file);
@@ -44,7 +23,21 @@ export const patchInscricao = (payload: IInscricaoDataReq) => {
     "nota_historico_posgraduacao",
     payload.nota_historico_posgraduacao_file.toString()
   );
-  return api.patch("/inscricoes", formData, {
+  return formData;
+};
+
+export const postInscricao = (payload: IInscricaoDataReq) => {
+  const reqData = formData(payload);
+  return api.post("/inscricoes", reqData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const patchInscricao = (payload: IInscricaoDataReq) => {
+  const reqData = formData(payload);
+  return api.patch("/inscricoes", reqData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
